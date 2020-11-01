9d06d8c63445959c33b51e985b0210fa7f85ff12
Vlad Khmelnitsky <ummmdanone@gmail.com>
2020-11-01 20:35:39 +0300

    feature: add solutions to some tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let array = [];
+  for (let i = 0; i < matrix.length; i++) {
+    array = array.concat(matrix[i]);
+  }
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "^^") count++;
+  }
+  return count;
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (
+      i === 0 &&
+      (arr[i] === "--discard-prev" || arr[i] === "--double-prev")
+    ) {
+      continue;
+    } else if (
+      i === arr.length - 1 &&
+      (arr[i] === "--discard-next" || arr[i] === "--double-next")
+    ) {
+      continue;
+    } else if (arr[i] === "--discard-next") {
+      i = i + 1;
+    } else if (arr[i] === "--discard-prev" && arr[i - 2] === "--discard-next") {
+      continue;
+    } else if (arr[i] === "--discard-prev") {
+      newArr.pop();
+    } else if (arr[i - 2] === "--discard-next" && arr[i] === "--double-prev") {
+      continue;
+    } else if (arr[i] === "--double-next") {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === "--double-prev") {
+      newArr.push(newArr[newArr.length - 1]);
+    } else {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
